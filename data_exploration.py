c848b46e4b602ac77468073e65e02680102e5756
Napoleon Koskinas <zenapolewn@gmail.com>
2016-06-11 01:05:33 +0100

    Exploring-Plotting data. Diffusion Framework draft

---
+++ data_exploration.py
@@ -0,0 +1,57 @@
+# Diffusion Maps Framework implementation as part of MSc Data Science Project of student 
+# Napoleon Koskinas at University of Southampton, MSc Data Science course
+
+import os, math
+import string
+
+import numpy as np
+from numpy import linalg as LA
+import pandas as pd
+
+import matplotlib
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+import datetime
+
+matplotlib.style.use('ggplot')
+
+
+datasource = './data/Science_ortholog_cluster_counts_all_taxa.xlsx'
+
+
+def main():
+	read_data()
+
+def read_data():
+
+	# Read data, get names of the 6 sheets, for different types of bacteria
+	xlData = pd.ExcelFile(datasource)
+	sheetNames = xlData.sheet_names
+
+	for name in sheetNames:
+		worksheet = xlData.parse(name)
+		# Keep only the actual timeseries data, last 30 columns
+		ts_data = worksheet.ix[:,-30:]
+
+		# Call a function which plot time-series data
+		timeseries_plot(ts_data)
+		break
+
+def timeseries_plot(data):
+	datetimeList = data.columns.values.tolist()
+	for index, dt in enumerate(datetimeList):
+		datetimeList[index] = dt.strftime('%m/%d/%Y %H:%M')
+
+	# print data.ix[1,:]
+	# exit()
+	# ts = pd.Series(data.ix[1,:], index=datetimeList)
+	ts = pd.Series(data.ix[0,:], index=datetimeList)
+
+	ts.plot()
+	plt.show()
+
+
+
+if __name__ == '__main__':
+	main()
--- diffusion_framework.py
+++ diffusion_framework.py
@@ -1,34 +1,89 @@
 # Diffusion Maps Framework implementation as part of MSc Data Science Project of student 
 # Napoleon Koskinas at University of Southampton, MSc Data Science course
 
+import os, math
+import string
+# from libc.math import sqrt, exp
+
 import numpy as np
 from numpy import linalg as LA
+import pandas as pd
+
+from sklearn.utils.extmath import fast_dot
+from sklearn.metrics.pairwise import pairwise_distances
+
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import AnnotationBbox, OffsetImage
-import os, math
-import pandas as pd
-import string
+
 
 datasource = './data/'
 
 
-
 def main():
-	read_data()
+	dataMatrix = np.zeros((3, 3))
+	print dataMatrix
+	exit()
 
-def read_data():
-	fileList = os.listdir(datasource)
-	for f in fileList:
-		xlData = pd.ExcelFile(''.join([datasource, f]))
+	# Calculate a matrix which returns pairwise distances 
+	# between two datapoints according to a metric
+	distMatrix = distance_matrix(dataMatrix)
 
-	sheetNames = xlData.sheet_names
+	# Choose a kernel function and create a kernel matrix
+	# with values according to a kernel function
+	kernelMatrix, totalDist = kernel_matrix(dMatrix)
 
-	for name in sheetNames:
-		print name
-		worksheet = xlData.parse(name)
-		print worksheet.head()
-		break
+	# Create probability transition matrix from kernel matrix and total dist.
+	probMatrix = markov_chain(kernel_matrix,totalDist)
+
+def distance_matrix(dataMatrix):
+
+	# Choose Euclidean distance
+	dMatrix = pairwise_distances(dataMatrix, metric = distance_metrics[2])
+
+	return dMatrix
+
+def kernel_matrix(dMatrix):
+	# Value of sigma is very important, and objective of research.Here default value.
+	sigma = 0.3
+
+	# Define a kernel matrix
+
+	# Get dimensions of square distance Matrix N
+	N = dMatrix.shape[0]
+
+	# Initialise with zeros Kernel matrix and distance vector
+	d = np.zeros(N)
+	K = np.zeros((N, N))
+
+	# Define Gaussian kernel : exp(-dMatrix(i,j)/(2*sigma**2))
+	for i in range(N):
+		# Optimise here, exclude computation under diagonal
+		for j in range(N):
+			K[i, j] = exp(- (dMatrix[i, j]**2)/(2*sigma**2))
+			d[i] += K[i,j]
+
+	return K, d
+
+
+def markov_chain(K, d):
+	N = K.shape[0]
+	# Initialise NxN probability transition matrix
+	P = np.zeros((N,N))
+
+	for i in range(N):
+		for j in range(N):
+			P[i, j] = K[i, j]/d[i]
+
+	# from future import division???
+
+	return P
+
+def svd():
+	pass
+
+def mapping():
+	pass
 
 if __name__ == '__main__':
 	main()
